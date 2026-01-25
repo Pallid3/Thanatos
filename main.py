22ef7bb4dab9b90d6252c80da55a9898c024e1d2
Pallid3 <43735493+Pallid3@users.noreply.github.com>
2026-01-25 23:37:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+import requests
+
+import oss
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+API_KEY = os.getenv('OSU_API_KEY')
+
+hander = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="+", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We are cute with {bot.user.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention}, don't use this word, you silly baka ! >:c")
+    
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    print(ctx)
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+# @tasks.loop(time=time(2, 0))   # runs 5 or 4 am in estonia cba
+@bot.command()
+async def req(ctx):
+    oss.make_user_request("kellad", API_KEY)
+    entries = oss.read_data()
+    await ctx.send(f"Data is: {entries}!")
+
+
+bot.run(token, log_handler=hander, log_level=logging.DEBUG)
---
+++ oss.py
@@ -0,0 +1,92 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+from datetime import datetime
+
+class OsuUser:
+    def __init__(self, data):
+        self.playcount = int(data["playcount"])
+        self.username = data["username"]
+        self.pp = float(data["pp_raw"])
+        self.timestamp = datetime.utcnow().isoformat()
+
+    def to_dict(self):
+        return {
+            "username": self.username,
+            "playcount": self.playcount,
+            "pp": self.pp,
+            "timestamp": self.timestamp
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        obj = cls.__new__(cls)
+        obj.username = data["username"]
+        obj.playcount = data["playcount"]
+        obj.pp = data["pp"]
+        return obj
+
+def make_user_request(username, API_KEY):
+    # username = "kellad"         # or a numeric user ID
+    username = str(username)
+    API_KEY = str(API_KEY)
+
+    url = "https://osu.ppy.sh/api/get_user"
+    params = {
+        "k": API_KEY,
+        "u": username,
+        "m": 0   # mode 0 = osu!standard
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    user_data = data[0]
+
+    filtered_data = OsuUser(user_data)
+    with open("osu_user.jsonl", "a") as f:
+        json.dump(filtered_data.to_dict(), f)
+        f.write("\n")
+    return
+
+def read_data():
+    entries = []
+    with open("osu_user.jsonl", "r") as f:
+        for line in f:
+            entries.append(json.loads(line))
+    return entries
+
+# load_dotenv()
+# token = os.getenv('DISCORD_TOKEN')
+# API_KEY = os.getenv('OSU_API_KEY')
+
+
+# make_user_request("kellad", API_KEY)
+
+# aaa = read_data()
+# print(aaa)
+# sorted_data = sorted(aaa, key=lambda x: x['timestamp'])
+# latest_two = sorted_data[-2:]
+# playcount1 = latest_two[0]['playcount']
+# playcount2 = latest_two[1]['playcount']
+
+
+# print("Latest two playcounts:", playcount1, playcount2)
+
+# if playcount2 > playcount1:
+#     print("Playcount increased by", playcount2 - playcount1)
+# elif playcount2 < playcount1:
+#     print("Playcount decreased by", playcount1 - playcount2)
+# else:
+#     print("Playcount did not change")
+
+
+
+
+
+# for e in entries:
+#     e["timestamp"] = datetime.fromisoformat(e["timestamp"])
+# latest = max(entries, key=lambda e: e["timestamp"])
+
+# print(latest)
+# print(latest["playcount"])
